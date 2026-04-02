1103a421e9aa7409b4972fc0f2549090eac812ab
MahdiarBanihashemrad <mahdiarbanihashem@gmail.comm>
2026-04-02 16:30:56 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+screen = Screen()
+
+screen.title("Write Your Name")
+screen.bgcolor("black")
+screen.setup(width= 1000, height=500)
+
+yertle = Turtle()
+yertle.color("red")
+
+yertle.pu()
+yertle.goto(-400, -100)
+
+
+
+
+
+screen.exitonclick()
