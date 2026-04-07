b1dca8d7687b56c7e35a1924c8527252c0708727
MahdiarBanihashemrad <mahdiarbanihashem@gmail.comm>
2026-04-07 15:26:40 -0500

    M complete

--- main.py
+++ main.py
@@ -7,13 +7,46 @@ screen.bgcolor("black")
 screen.setup(width= 1000, height=500)
 
 yertle = Turtle()
-yertle.color("red")
 
-yertle.pu()
-yertle.goto(-400, -100)
+yertle.pensize(3)
+yertle.speed(3)
+
+def move(x, y):
+    yertle.pu()
+    yertle.goto(x, y)
+    yertle.pd()
+
+def M(x, y):
+    yertle.color("red")
+    move(x, y)
+    yertle.setheading(90)
+    yertle.forward(100)
+    yertle.right(150)
+    yertle.forward(60)
+    yertle.left(120)
+    yertle.forward(60)
+    yertle.right(150)
+    yertle.forward(100)
+
+
+def A(x, y):
+    yertle.color("green")
+    move(x, y)
+    yertle.circle(20)
+    move(x+40, y)
+    yertle.setheading(90)
+    yertle.forward(40)
 
 
 
+
+
+
+
+
+
+M(-300, 0)
+A(-200, 0)
 
 
 screen.exitonclick()
